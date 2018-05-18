9aff4ff5158ff79002042056a8547b3d258612a3
Ruben Torres Lima <ruben_jtl@hotmail.com>
2018-05-18 00:38:09 -0500

    Create vecAdd.sh

---
+++ CUDA/vecAdd/vecAdd.sh
@@ -0,0 +1,44 @@
+for i in {1..5}
+do
+    ./vecAdd.out 1 1000	
+done
+for i in {1..5}
+do
+    ./vecAdd.out 2 1000	
+done
+for i in {1..5}
+do
+    ./vecAdd.out 4 1000	
+done
+for i in {1..5}
+do
+    ./vecAdd.out 8 1000	
+done
+for i in {1..5}
+do
+    ./vecAdd.out 16 1000	
+done
+for i in {1..5}
+do
+    ./vecAdd.out 32 1000	
+done
+for i in {1..5}
+do
+    ./vecAdd.out 64 1000	
+done
+for i in {1..5}
+do
+    ./vecAdd.out 128 1000	
+done
+for i in {1..5}
+do
+    ./vecAdd.out 256 1000	
+done
+for i in {1..5}
+do
+    ./vecAdd.out 512 1000	
+done
+for i in {1..5}
+do
+    ./vecAdd.out 1024 1000	
+done
